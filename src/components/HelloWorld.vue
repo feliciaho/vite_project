5b544c3136529a9b7db9e4a86a5fbd83567d642e
unknown <felicia18felicia18@gmail.com>
2025-03-05 23:42:03 +0800

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,6 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <div></div>
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
