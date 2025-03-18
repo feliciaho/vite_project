3c7a2c4531c4208d5e08adb9de24449572a6e003
feliciaho <feliciaho@mangot5.com>
2025-03-18 17:07:00 +0800

    250318

---
+++ src/views/mapView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>mapState & mapActions</div>
+  <div>
+    {{ name }}
+  </div>
+  <div>
+    {{ getUserName }}
+  </div>
+  <button type="button" @click="updateName">click</button>
+</template>
+<script>
+  import { mapState, mapActions } from 'pinia';
+  import userStore from '@/stores/user';
+
+  export default {
+  // 帶入state或是getters
+    computed:{
+      ...mapState(userStore, ['name', 'getUserName']),
+      ...mapActions(userStore, ['updateName'])
+    }
+  };
+</script>
